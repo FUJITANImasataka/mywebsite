9e4da7c6dd54159843d0b167eacc650294593b3b
FUJITANImasataka <startallover2008@yahoo.co.jp>
2023-02-03 07:44:59 +0000

    BGM ver0.0.0

---
+++ resources/views/index.blade.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>JobTribes scholar wanted</title>
+        <meta name="description" content="jobtribes scholar recruiting">
+        <link rel="icon" href="images/favicon.svg" type="image/svg+xml">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS -->
+        <link rel="stylesheet" href="https://unpkg.com/destyle.css@1.0.5/destyle.css">
+        <link href="css/style.css" rel="stylesheet">
+    
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/f88a8a6c38.js" crossorigin="anonymous"></script>
+    </head>
+
+	<body>
+        <header>
+            <div class="wrapper">
+                <a class="logo" href="#hero">JobTribes scholar wanted</a>
+                <nav>
+                    <button class="btn-menu">Menu</button>
+                    <ul class="main-nav">
+                        <li><a href="#conditions">Conditions</a></li>
+                        <li><a href="#news">News</a></li>
+                        <li><a href="#owner">Owner</a></li>
+                        <!--<li><a href="#contact">Contact</a></li>-->
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <section id="hero">
+            <div class="wrapper">
+                <h1>Jobtribes Scholar Wanted</h1>
+                <p class="hero-name"><i class="fa-brands fa-discord"></i>　Jack and lovely Friends</p>
+                <p>
+                    Jack and lovely Friends is a Discord community of SatoshiAMANO, the individual owner of jobTribes scholarship. 
+                    The JobTribes Scholarship allows NFT owners to lend out their NFT-enhanced Amulet, and other players (Scholars) who do not own NFTs can borrow them to play the game.
+                    <br><br>
+                    <!-- ジャックと愉快な仲間たちは、ブロックチェーン連動ゲームJobTribesのscholarship個人オーナー、SatoshiAMANOの<i class="fa-brands fa-discord"></i>Discordコミュニティーです。
+                    JobTribesのScholarshipでは、NFTの持ち主（オーナー）はNFTで強化されたAmuletを貸し出し、NFTを保有していない、他のプレイヤー（スカラー）はそれを借りてゲームプレイをすることが可能になります。 -->
+                <figure>
+                    <figcaption>BGM：MaouDamashii</figcaption>
+                    <audio
+                        preload="metadata"
+                        loop
+                        controls
+                        src="/audio/bgm.mp3"
+                    >
+                        
+                    </audio>
+                </figure>
+                
+                </p>
+            </div>
+        </section>
+
+        <section id="conditions" class="wrapper">
+            <h2>conditions</h2>
+            <p>
+                DEP owner:scholar = 50:50<br>
+                Transfer Handling Fee 850DEP = 50:50<br>
+                Decks to be lent = Common and Rare Decks (both)<br>
+                Piggybank level = lv.30<br>
+                Train and promote the deck as long as you can.<br>
+                Play NFT Quest every day.<br>
+                <!--If u forget to play NFT quest without prior notice, your scholarship will be terminated at that time.-->
+                <!--u'll be responsible for all shipping charges in that case.<br>-->
+                <!--Weely report: Post screenshots of your decks every week.<br>-->
+                u can get a chance to play an epic deck if u join us!
+            </p>
+            <a href="https://jobtribes.playmining.com/news/15297/"><img src="images/nftquest.png" alt="nftquest"></a>
+        </section>
+
+        <section id="news">
+            <h2>News</h2>
+            <div class="wrapper">
+                <table class="news-table">
+                    <tr>
+                        <td class="news-date">2/xx/2023</td>
+                        <td class="news-content">secret</td>
+                    </tr>
+                    <tr>
+                        <td class="news-date">2/1/2023</td>
+                        <td class="news-content">season12 started</td>
+                    </tr>
+                    <tr>
+                        <td class="news-date">2/1/2023</td>
+                        <td class="news-content">web page opened</td>
+                    </tr>
+                </table>
+            </div>
+        </section>
+
+        <section id="owner" class="wrapper">
+            <h2>owner</h2>
+            <div class="owner-list">
+                <div class="owner-text">
+                    <h3>SatoshiAMANO</h3>
+                    <p>
+                        "HikiNeet" 『代表戸締役』<br>
+                        <!--<ul class="contact-sns">-->
+                        <!--    <li><a href="https://twitter.com/Amano295"><i class="fab fa-twitter"></i></a></li>-->
+                        <!--</ul>-->
+                    </p>
+                </div>
+                <button type="button" onclick="location.href='https://twitter.com/Amano295'"><img src="images/satoshi.jpg" alt="SatoshiAMANO"></button>
+            </div>
+        </section>
+
+        <section id="contact">
+            
+            <button type=“button” onclick="location.href='https://ecf402fb130845a3a99b723d9d53cafb.vfs.cloud9.ap-northeast-1.amazonaws.com/register'" class="contact-btn contact-form">Apply meow🎵</button>
+            
+            <!--<h2>Contact</h2>-->
+            <!--<form class="contact-form" action="">-->
+                <!-- <label>Name</label>
+            <!--    <input class="contact-field" type="text" placeholder="John Doe">-->
+            <!--    <label>Email</label>-->
+            <!--    <input class="contact-field" type="email" placeholder="hello@example.com"> -->
+            <!--    <input class="contact-btn" type="submit" value="Apply Now!">-->
+            <!--</form>-->
+
+            <div class="footer wrapper">
+                <p>
+                    <!-- <a href="mailto:info@example.om">お問い合わせ</a> /  -->
+                    <small>&copy; Jack and lovely Scholars</small>
+                </p>
+            </div>
+        </section>
+
+    <!-- JavaScript -->
+        <script src="{{ asset('/js/script.js') }}"></script>
+	</body>
+</html>
