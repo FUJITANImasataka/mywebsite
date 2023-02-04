621249935703e59e7fee52e51e86722b5ad6880b
FUJITANImasataka <startallover2008@yahoo.co.jp>
2023-02-04 04:23:19 +0000

    indivisual post

--- resources/views/board/show.blade.php
+++ resources/views/board/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Individual postings
+            Individual posting
         </h2>
 
         <x-message :message="session('message')" />
