039d89e84961994eb1c6c914c3ac16f755be1189
FUJITANImasataka <startallover2008@yahoo.co.jp>
2023-02-07 08:43:40 +0000

    logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 <div class="shrink-0 flex items-center">
-    <a href="{{ route('board.index') }}">
+    <a href="https://jobtribes.playmining.com/" target="_blank">
         <img src="{{asset('logo/logo.png')}}" style="max-height:80px;">
     </a>
 </div>
